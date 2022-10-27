77d1627bc9adb6a415ce00f1873588908ba7eab2
Borna Cvitanić <borna.cvitanic@gmail.com>
2022-10-27 13:27:24 +0200

    Create GithubAPIController.cs

---
+++ BornaCvitanicPortfolioWebapp/Controllers/GithubAPIController.cs
@@ -0,0 +1,24 @@
+﻿using BornaCvitanicPortfolioWebapp.Models.GithubAPI;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BornaCvitanicPortfolioWebapp.Controllers
+{
+    public class GithubAPIController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Repository> repos;
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("User-Agent", "bornacvitanic-portfolio-webapp");
+                using (var response = await httpClient.GetAsync("https://api.github.com/users/bornacvitanic/repos"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    repos = JsonConvert.DeserializeObject<List<Repository>>(apiResponse);
+                }
+            }
+            return View(repos);
+        }
+    }
+}
