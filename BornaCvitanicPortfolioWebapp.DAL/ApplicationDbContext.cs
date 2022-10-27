a374b238bc235667c4192e5577f5d6e74b8b0e32
Borna Cvitanić <borna.cvitanic@gmail.com>
2022-10-27 18:14:26 +0200

    Add AddedPosts migration

--- BornaCvitanicPortfolioWebapp.DAL/ApplicationDbContext.cs
+++ BornaCvitanicPortfolioWebapp.DAL/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BornaCvitanicPortfolioWebapp.Model.Portfolio;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BornaCvitanicPortfolioWebapp.DAL
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Post> Posts { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
