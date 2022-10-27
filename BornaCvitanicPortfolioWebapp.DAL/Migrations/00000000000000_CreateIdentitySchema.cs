294ae8515bc1687703dbe3486faded9d07b64e49
Borna Cvitanić <borna.cvitanic@gmail.com>
2022-10-27 18:07:33 +0200

    Fix ApplicationDbContext namespace

--- BornaCvitanicPortfolioWebapp.DAL/Migrations/00000000000000_CreateIdentitySchema.cs
+++ BornaCvitanicPortfolioWebapp.DAL/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace BornaCvitanicPortfolioWebapp.Data.Migrations
+namespace BornaCvitanicPortfolioWebapp.DAL.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
