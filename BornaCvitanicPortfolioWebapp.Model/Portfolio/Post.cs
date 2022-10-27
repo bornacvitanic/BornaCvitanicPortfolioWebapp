ff8803f2be4d9662564f3d56b6d727fe45ff7a66
Borna Cvitanić <borna.cvitanic@gmail.com>
2022-10-27 17:48:35 +0200

    Create Post.cs

---
+++ BornaCvitanicPortfolioWebapp.Model/Portfolio/Post.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BornaCvitanicPortfolioWebapp.Models.Portfolio
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
