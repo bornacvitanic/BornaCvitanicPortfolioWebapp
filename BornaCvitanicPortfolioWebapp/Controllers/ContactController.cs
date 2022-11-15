0246178540d2157350ebdffd5ed1218d2daf09af
Borna Cvitanić <borna.cvitanic@gmail.com>
2022-11-15 14:53:38 +0100

    Add Contact page

---
+++ BornaCvitanicPortfolioWebapp/Controllers/ContactController.cs
@@ -0,0 +1,55 @@
+﻿using BornaCvitanicPortfolioWebapp.Models.Contact;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Globalization;
+
+namespace BornaCvitanicPortfolioWebapp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.FillDropdownValues();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        private void FillDropdownValues()
+        {
+            var selectItems = new List<SelectListItem>();
+
+            CultureInfo[] getcultureinfo = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+            
+            List<string> Culturelist = new List<string>();
+            
+            foreach (CultureInfo getculture in getcultureinfo)
+            {
+                RegionInfo getregininfo = new RegionInfo(getculture.LCID);
+                if (!(Culturelist.Contains(getregininfo.EnglishName)))
+                {
+                    Culturelist.Add(getregininfo.EnglishName);
+                }
+            }
+            Culturelist.Sort();
+
+            selectItems.Add(new SelectListItem("Please select", ""));
+            int index = 1;
+            foreach (var item in Culturelist)
+            {
+                selectItems.Add(new SelectListItem(item, index.ToString()));
+                index++;
+            }
+
+            ViewBag.CountryItems = selectItems;
+        }
+    }
+}
---
+++ BornaCvitanicPortfolioWebapp/Models/Contact/ContactModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BornaCvitanicPortfolioWebapp.Models.Contact
+{
+    public class ContactModel
+    {
+        [Required]
+        [StringLength(500, MinimumLength = 1)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(500, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+    }
+}
